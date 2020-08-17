0d20a24149b5775f25fc2f40dca598c14dc0636e
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-17 15:08:25 -0500

    question 1 solved of classes

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee{
+  constructor (first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
